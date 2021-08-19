86d8e7a5e810b8f67daf7ede7965b648e5e99dd7
fabio-n19 <fmigueln17@gmail.com>
2021-08-18 23:28:57 -0600

    I fucking did it.. my first script! From R to GitHub

---
+++ Borrar R.R
@@ -0,0 +1,13 @@
+print("This file was created within RStudio")
+help.search(print)
+
+print("And now it lives on GitHub")
+
+library(tidyverse)
+library(bit)
+install.packages(bit)
+
+"This is so I can see if I learned well how to stage and commit"
+
+
+
